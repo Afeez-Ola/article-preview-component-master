1ae9273bb3043bbb06093f32eb912b59a0272250
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-10-24 15:01:11 +0200

    ahh, this thing challenge me small oo

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ let anotherContent = document.querySelector('.profile');
 let profileSection = document.querySelector('.profile_section')
 
 toggleLink.addEventListener('click', (e) => {
- event.preventDefault();
- anotherContent.style.display = 'none';
- 
- if (content.style.display === 'none') {
-  content.style.display = 'flex';
-  profileSection.style.backgroundColor = "#48556A"
-  anotherContent.style.display = 'none';
- } else {
-  content.style.display = 'none';
-  anotherContent.style.display = 'flex';
-  profileSection.style.backgroundColor = "white"
- }
+    e.preventDefault(); 
+
+    if (window.innerWidth < 768) { 
+        anotherContent.style.display = 'none';
+
+        if (content.style.display === 'none') {
+            content.style.display = 'flex'; 
+            profileSection.style.backgroundColor = "#48556A"; 
+            anotherContent.style.display = 'none'; 
+        } else {
+            content.style.display = 'none'; 
+            anotherContent.style.display = 'flex'; 
+            profileSection.style.backgroundColor = "white";
+        }
+    } else{
+      // anotherContent.style.display = 'none';
+
+        if (content.style.display === 'none') {
+            content.style.display = 'flex'; 
+            // profileSection.style.backgroundColor = "#48556A"; 
+            // anotherContent.style.display = 'none'; 
+        } else {
+            content.style.display = 'none'; 
+            anotherContent.style.display = 'flex'; 
+            profileSection.style.backgroundColor = "white";
+            content.style.width = "40%"
+        }
+    }
 });
